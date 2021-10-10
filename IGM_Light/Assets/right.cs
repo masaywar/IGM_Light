458d4a12d3ace65d338d8fb3e482904b49d00e1c
mini102 <fysbis1024@gmail.com>
2021-10-10 23:10:45 +0900

    first

---
+++ IGM_Light/Assets/right.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class right : MonoBehaviour//, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+       // Debug.Log("right");
+        player.transform.position += new Vector3(3.0f, 0, 0);
+    }
+}
