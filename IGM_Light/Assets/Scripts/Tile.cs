e7b9a722f6cc3f9022e9ac4d05ec6566e4691109
masaywar <masaywar@gmail.com>
2021-10-12 00:31:38 +0900

    Tile 작업 시작 1. 타일 배치 구현 생각하기

---
+++ IGM_Light/Assets/Scripts/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int Column
+    {
+        get;
+        private set;
+    }
+
+    public int Row
+    {
+        get;
+        private set;
+    }
+
+    [SerializeField] private int col, row;
+
+    private void Update()
+    {
+        col = Column;
+        row = Row;
+    }
+
+    public void SetTilePosition(int row, int column)
+    {
+        Row = row;
+        Column = column;    
+    }
+}
---
+++ IGM_Light/Assets/Scripts/TileManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager: MonoBehaviour
+{
+    [SerializeField] private Tile _baseTile;
+    [SerializeField] private int _length;
+    public int Length
+    {
+        get=>_length;
+    }
+    
+    [SerializeField] private Tile[] _tiles;
+
+    public Tile[] Tiles
+    {
+        get 
+        {
+            if(_tiles == null)
+            {
+                _tiles = new Tile[Length*Length];
+            }
+
+            return _tiles;
+        }
+    } 
+
+   
+
+    private void Awake()
+    {
+        TryInitialize();
+    }
+
+    private void TryInitialize()
+    {
+        _tiles = new Tile[Length*Length];
+        SpawnTiles();
+    }
+
+    private void SpawnTiles()
+    {
+        for (int idx=0; idx < Length*Length; idx++)
+        {
+            Tiles[idx] = Instantiate(_baseTile, transform);
+            Tiles[idx].SetTilePosition(idx/Length, idx%Length);
+        }
+    }
+}
