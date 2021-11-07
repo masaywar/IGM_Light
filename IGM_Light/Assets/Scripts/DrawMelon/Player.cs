b03ba416007a0c5135ce10749e19cec73b4e5cf1
Chaemin <fysbis1024@gmail.com>
2021-11-08 00:41:25 +0900

    animation

--- IGM_Light/Assets/Scripts/DrawMelon/Player.cs
+++ IGM_Light/Assets/Scripts/DrawMelon/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
@@ -11,15 +12,41 @@ public class Player : MonoBehaviour
     public int col = 0;
     public ColorType _color;
     public int mov = 0;
+    private CustomTile onTile;
     // public ColorType standard;
 
     private int _length;
+    public bool IsUp;
+
+    public Animator animator;
+
+    enum States{
+        up = 1,
+        down = 2,
+        left = 3,
+        right = 4,
+        idle = 0
+    }
+
+    enum Colors
+    {
+        Basic = 0,
+        Red = 1,
+        Blue = 2,
+        Cyan = 3,
+        Yellow = 6,
+        Pink = 7,
+        Purple = 8,
+        Green = 9
+    }
     // Start is called before the first frame update
     void Start()
     {
         m_boardManager = transform.parent.GetComponent<BoardManager>();
 
         _length = m_boardManager.Length;
+
+        //GetComponent<Animator>().SetInteger("direction",1);
     }
 
     // Update is called once per frame
@@ -30,57 +57,74 @@ public class Player : MonoBehaviour
         {
             Draw();
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow)&&Input.GetAxis("Vertical")>0)
         {
+            Debug.Log("up");
+            GetComponent<Animator>().SetInteger("direction", (int)States.up);
             row = TryMove(row-1, col) ? row-1 : row;
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow)&&Input.GetAxis("Vertical")<0)
         {
+           GetComponent<Animator>().SetInteger("direction", (int)States.down);
             row = TryMove(row+1, col) ? row+1 : row;
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+           GetComponent<Animator>().SetInteger("direction", (int)States.left);
             col = TryMove(row, col-1) ? col-1 : col;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+           Debug.Log("right");
+           GetComponent<Animator>().SetInteger("direction", (int)States.right);
             col = TryMove(row, col+1) ? col+1 : col;
         }
+        else if(onTile!=null){
+           if (this.transform.position == onTile.transform.position)  //move 확인
+            {
+                //Debug.Log("arrive");
+                GetComponent<Animator>().SetInteger("direction", (int)States.idle);
+            }
+         }
 #else
-        
-#endif
 
+#endif
     }
 
     public void Move(Vector2 direction)
     {
+
         if (direction.x == -1)
         {
-            row = TryMove(row-1, col) ? row-1 : row;
+            GetComponent<Animator>().SetFloat("direction_x", direction.x);
+            row = TryMove(row-1, col) ? row-1 : row;  //up
         }
         else if (direction.x == 1)
         {
-            row = TryMove(row+1, col) ? row+1 : row;
+            GetComponent<Animator>().SetFloat("direction_x", direction.x);
+            row = TryMove(row+1, col) ? row+1 : row;  //down
         }
         else if (direction.y == -1)
         {
-            col = TryMove(row, col-1) ? col-1 : col;
+            GetComponent<Animator>().SetFloat("direction_y", direction.y);
+            col = TryMove(row, col-1) ? col-1 : col;  //left
         }
         else if (direction.y == 1)
         {
-            col = TryMove(row, col+1) ? col+1 : col;
+            GetComponent<Animator>().SetFloat("direction_y", direction.y);
+            col = TryMove(row, col+1) ? col+1 : col;  //right
         }
     }
 
     public bool TryMove(int p_row, int p_col)
     {
-        if (m_boardManager.TryGetTile(p_row, p_col, out var onTile))  //onTile에는 현재위치
+        if (m_boardManager.TryGetTile(p_row, p_col, out onTile))  //onTile에는 갈 위치
         {
             if (onTile.HasObstacle)
                 return false;
 
             mov++;
-            this.transform.position = onTile.transform.position;
+            this.transform.DOMove(onTile.transform.position,2); //애니메이션
 
             if (onTile.HasFilter)
             {
@@ -88,7 +132,6 @@ public class Player : MonoBehaviour
                 onTile.Filter.gameObject.SetActive(false);
                 ChangeColor(_color);
             }
-
             return true;
         }
 
@@ -97,12 +140,38 @@ public class Player : MonoBehaviour
 
     void ChangeColor(ColorType color)
     {
-
-        if (m_boardManager.TryGetCharacterSprite(color, 6, out var sprite)) //6은 stand
+        switch (color)
+        {
+            case ColorType.Basic:
+                GetComponent<Animator>().SetInteger("color", (int)Colors.Basic);
+                break;
+            case ColorType.Red:
+                GetComponent<Animator>().SetInteger("color", (int)Colors.Red);
+                break;
+            case ColorType.Blue:
+                GetComponent<Animator>().SetInteger("color", (int)Colors.Blue);
+                break;
+            case ColorType.Cyan:
+                GetComponent<Animator>().SetInteger("color", (int)Colors.Cyan);
+                break;
+            case ColorType.Green:
+                GetComponent<Animator>().SetInteger("color", (int)Colors.Green);
+                break;
+            case ColorType.Pink:
+                GetComponent<Animator>().SetInteger("color", (int)Colors.Pink);
+                break;
+            case ColorType.Purple:
+                GetComponent<Animator>().SetInteger("color", (int)Colors.Purple);
+                break;
+            case ColorType.Yellow:
+                GetComponent<Animator>().SetInteger("color", (int)Colors.Yellow);
+                break;
+        }
+        /*if (m_boardManager.TryGetCharacterSprite(color, 6, out var sprite)) //6은 stand
         {
             SpriteRenderer spriteR = gameObject.GetComponent<SpriteRenderer>();
             spriteR.sprite = sprite;
-        }
+        }*/
 
     }
     public void Draw()  //필터의 색과 같은 Tile 색,캐릭터 색 변경
