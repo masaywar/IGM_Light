59d28687bdf0db89f06cdd4abafff9739dc549fa
Masaywar <dngus0715@ajou.ac.kr>
2021-11-28 03:31:02 +0900

    world stage select - scroll view added

---
+++ IGM_Light/Assets/Scripts/DrawMelon/UI/WorldSelectScrollRect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldSelectScrollRect : MonoBehaviour
+{
+    private Transform _pivot;
+    [SerializeField]private RectTransform[] _worlds;
+    private Vector3[] _contentPositions;
+
+    private Vector3 _center;
+
+    private bool isInit = false;
+
+}
+
+  
