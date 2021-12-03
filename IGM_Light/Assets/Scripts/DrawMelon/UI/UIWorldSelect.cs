fb500fc64c648a3049992602eb176fff9cbff80d
masaywar <dngus0715@naver.com>
2021-12-03 21:43:22 +0900

    mod world select

--- IGM_Light/Assets/Scripts/DrawMelon/UI/UIWorldSelect.cs
+++ IGM_Light/Assets/Scripts/DrawMelon/UI/UIWorldSelect.cs
@@ -10,29 +10,23 @@ using Lean.Touch;
 
 public class UIWorldSelect : UIWindow
 {
-    private Image _backGroundImage;
+    private Image[] _worldImages;
     private UIManager _uiManager;
     public SpriteDatabaseLoader SpriteDatabase;
 
-    [SerializeField]private int _showingIndex = 0;
     [SerializeField] private Transform _worldContainer;
-    [SerializeField] private Transform[] _buttons;
-
-    public float Threshold;
-    
-    private LeanFingerSwipe _fingerSwipe;
-    private LeanTouch _leanTouch;
 
     private float _distance;
     protected override void Awake()
     {
         _uiManager = UIManager.Instance;
 
-        _backGroundImage = GetComponent<Image>();
-        _backGroundImage.sprite = SpriteDatabase.BackgroundSprites[_showingIndex].sprites[UserDataInstance.Instance.WorldsLastClearData[_showingIndex]];
-    
-        _fingerSwipe = FindObjectOfType<LeanFingerSwipe>();
-        _leanTouch = FindObjectOfType<LeanTouch>();
+        for(int k=0; k < SpriteDatabase.WorldsSprites.Count; k++)
+        {
+            int index = k;
+            _worldImages[k].sprite = SpriteDatabase.WorldsSprites[k].sprites[UserDataInstance.Instance.WorldsLastClearData[k]];
+            _worldImages[k].GetComponent<Button>().onClick.AddListener(()=>OnSelectWorld(index+1));
+        }    
 
         base.Awake();
     }
@@ -40,8 +34,6 @@ public class UIWorldSelect : UIWindow
     public override void Close()
     {
         _worldContainer.localPosition = Vector3.zero;
-        _fingerSwipe.enabled = true;
-        _leanTouch.enabled = true;
         base.Close();
     }
 
@@ -59,54 +51,12 @@ public class UIWorldSelect : UIWindow
         }
     }
 
-    public void SetNextWorld()
+    public void OnSelectWorld(int index)
     {
-        if (_showingIndex < SpriteDatabase.BackgroundSprites.Count-1)
-            _backGroundImage.sprite = SpriteDatabase.BackgroundSprites[++_showingIndex].sprites[UserDataInstance.Instance.WorldsLastClearData[_showingIndex]];
-    } 
-
-    public void SetPrevWorld()
-    {
-        if (_showingIndex != 0)
-            _backGroundImage.sprite = SpriteDatabase.BackgroundSprites[--_showingIndex].sprites[UserDataInstance.Instance.WorldsLastClearData[_showingIndex]];
-    }
-    public void OnSelectWorld()
-    {
-        UserDataInstance.Instance.CurrentWorld = _showingIndex + 1;
+        UserDataInstance.Instance.CurrentWorld = index;
 
         var uiStage = _uiManager.GetWindow<UIStageSelect>("UIStageSelect");
         uiStage.World = UserDataInstance.Instance.CurrentWorld;
         uiStage.Open(true);
-    }
-
-    #if UNITY_EDITOR
-    private void Update()
-    {
-
-    }
-    #endif
-
-    public void OnSwipe(Vector3 swipe)
-    {
-        if(_distance <= Threshold)
-        {
-            OnSelectWorld();
-            return;
-        }
-
-        if (swipe.x < 0)
-        {
-            SetNextWorld();
-        }
-
-        else
-        {
-            SetPrevWorld();
-        }
-    }
-
-    public void OnDistance(Single distance)
-    {
-        _distance = distance;
     }
 }
